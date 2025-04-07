75272360911460ddac310cc78c4e67edc4c2518d
Renz Christian Cabato <renzcabato@Renzs-Mac-mini.local>
2025-04-07 20:38:25 +0800

    Done

--- database/migrations/2025_04_07_123526_create_groups.php
+++ database/migrations/2025_04_07_123526_create_groups.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateDocumentsTable extends Migration
+class CreateGroups extends Migration
 {
     /**
      * Run the migrations.
@@ -13,20 +13,20 @@ class CreateDocumentsTable extends Migration
      */
     public function up()
     {
-        Schema::create('documents', function (Blueprint $table) {
+        Schema::create('groups', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('title');
             $table->string('group');
-            $table->text('content');
             $table->timestamps();
         });
-
-        // Add full-text index for better search
-        \DB::statement('ALTER TABLE documents ADD FULLTEXT(content)');
     }
 
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
     public function down()
     {
-        Schema::dropIfExists('documents');
+        Schema::dropIfExists('groups');
     }
 }
